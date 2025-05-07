e461407a8d8e8c80dc9c4470bcd6697efce235a9
Adrian Gidaro <adriangidaro@gmail.com>
2025-05-07 19:27:42 +1000

    Class lifecycle and state rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,34 @@ import './App.css'
 
 class App extends React.Component {
 
+  constructor(){
+    super();
+
+    this.state = {
+      pokemonId: 0,
+      pokemonName: ''
+    }
+  }
+
   componentDidMount(){
     console.log('Component mounted.')
+  }
+
+  componentDidUpdate(){
+    console.log(this.state)
   }
 
   render() {
     return (
       <>
         <h1>This is a class component!</h1>
+
+        {this.state.pokemonName > 0 && 
+          <h1>
+            {this.state.pokemonName}
+          </h1>
+        }
+
       </>
     )
   }
