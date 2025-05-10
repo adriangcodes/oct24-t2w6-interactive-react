662de71429ff3018cd74d3ccd7d41b92067860f7
Adrian Gidaro <adriangidaro@gmail.com>
2025-05-10 12:26:47 +1000

    feat: added page links nav

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import PokemonSearcher from './components/PokemonSearcher.jsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
 import { BaseLayout } from './pages/layouts/BaseLayout.jsx'
 
 createRoot(document.getElementById('root')).render(
--- src/pages/layouts/BaseLayout.jsx
+++ src/pages/layouts/BaseLayout.jsx
@@ -1,14 +1,33 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 export function BaseLayout(){
     return(
         <>
-            <header>
-                <nav>
-                    <h1>Cool Pokemon Website</h1>
-                </nav>
-            </header>
+			<header>
+				<h1>Cool Pokemon Website</h1>
+				<nav>
+					<ul>
+						<li>
+							<Link to={"/"}>
+								Home
+							</Link>
+						</li>
+						<li>
+							<Link to={"/about"}>
+								About
+							</Link>
+						</li>
+						<li>
+							<Link to={"/pokemon"}>
+								Pokemon Search
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</header>
+
             <Outlet />
+
             <footer>
                 Copyright Me Because I'm Awesome!
             </footer>
