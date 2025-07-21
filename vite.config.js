555ae82c4bd02025901ff2942ec90f5f6f1a239c
Adrian Gidaro <adriangidaro@gmail.com>
2025-07-21 19:54:32 +1000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://adriangcodes.github.io/oct24-t2w6-interactive-react/"
+  base: "/oct24-t2w6-interactive-react/"
 })
